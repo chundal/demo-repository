2d77a459961a929e906f97289272888f80a124f8
Lee Jeong Min <gcburkey37@gmail.com>
2023-06-17 14:25:14 +0900

    백엔드 개발 16. @Repository 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.service.TodoService;
 
@@ -16,7 +17,6 @@ import com.example.demo.service.TodoService;
 public class TestController {
 	@Autowired
 	private TodoService service;
-	
 	@GetMapping("/test")
 	public ResponseEntity<?>testTodo(){
 		String str = service.testService();
